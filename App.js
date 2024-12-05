f900c7efc04876b8b2d139c74a6f7aff0bfbf875
Asit Patel <patela8@myumanitoba.ca>
2024-12-05 14:12:05 -0600

    fix main() stackoverflow and fix http error handling

--- App.js
+++ App.js
@@ -3,31 +3,38 @@ import axios from 'axios';
 import crypto from 'crypto';
 import { questions, API_AUTH_URL, API_BASE_URL } from './constants.js';
 
+let terminateCLI = false;
+main();
 
-runApp();
+async function main(){
 
-async function runApp(){
-  inquirer.prompt(questions).then(async answers => {
-    // let userID, logicToken, dbToken;
-
+  while(!terminateCLI){
+    let answers = await inquirer.prompt(questions);
     console.log("Please wait while we log you in...");
     let {userID, logicToken, dbToken} = await getLoginCredentials(answers.email, answers.password);
-    console.log("Login successful. Fetching your Posts...");
 
-    let headers = createHeaders(userID, logicToken, dbToken);
+    // login was successful
+    if(userID && logicToken && dbToken){
+      console.log("Login successful. Fetching your Posts...");
+      await getPosts(userID, logicToken, dbToken);
+    }
+  }
+}
+
+async function getPosts(userID, logicToken, dbToken){
+  let headers = createHeaders(userID, logicToken, dbToken);
     let url = `${API_BASE_URL}/api/Posts/${userID}`;
     
-    axios.get(url, {headers: headers})
+    await axios.get(url, {headers: headers})
     .then(function (response) {
       console.log("\nYour Posts: ");
       console.log(response.data);
+      terminateCLI = true;
     })
     .catch(function (error) {
-      handleError(error.response.status, error.response.statusText, error.response.data);
+      handleError(error);
     });
-  });
 }
-
 
 async function getLoginCredentials(email, password){
   let url = API_AUTH_URL + "/api/Users/PasswordLogin";
@@ -45,7 +52,7 @@ async function getLoginCredentials(email, password){
     dbToken = response.data.db_token;
   })
   .catch(function (error) {
-    handleError(error.response.status, error.response.statusText, error.response.data);
+    handleError(error);
   });
 
   return {userID, logicToken, dbToken};  
@@ -65,13 +72,28 @@ function hashPassword(passwordStr){
   return crypto.createHash('sha256').update(passwordStr).digest('hex');
 }
 
-function handleError(statusCode, statusMessage, responseData){
-  console.log("[Error: " + statusCode + " " + statusMessage + "] " + responseData);
-  if(statusCode == 500 || statusCode == 404){
-    console.log("Please try again later. Exiting...");
-    process.exit(1); // Exit with error
-  }else if(statusCode == 400 || statusCode == 401){
-    console.log("Please try again...");
-    runApp();
+function handleError(error){
+  if (error.code === 'ENOTFOUND') {
+    console.error("Error: Invalid URL or the domain cannot be reached.");
+    exitApp();
+  } else if (error.response) {
+    // The server responded with a status code outside the range of 2xx
+    console.log("[Error: " + error.response.status + " " + error.response.statusText + "] " + error.response.data);
+    
+    if(error.response.status == 400 || error.response.status == 401){
+      console.log("Please try again...");
+    }else{
+      console.error("Please try again later.");
+      exitApp();
+    }
+  } else {
+    // Something else happened
+    console.error("Unexpected error:", error.message);
+    exitApp();
   }
+}
+
+function exitApp(){
+  console.log("Exiting...");
+  terminateCLI = true;
 }
